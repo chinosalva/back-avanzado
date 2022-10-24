eed1cd6a28d57e1c365b733ddee46a29b7ccba7b
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-10-24 14:19:03 -0500

    CRUD completo

--- index.js
+++ index.js
@@ -11,26 +11,28 @@ mongoose.connect(process.env.DB_URI, async() => {
         CRUD con mongoose
     */
 
-    const user = await User.create({
-        address: 'Direcion prueba',
-        birthday: new Date(),
-        contacts: ['Pedrito', 'Jorge'],
-        email: 'chinito@gmail.com',
-        lastname: 'Rodriguez',
-        name: 'Juan',
-        password: '123',
-        phonenumber: '5627572781',
-        role: 'Client',
-    })
+    // const user = await User.create({
+    //     address: 'Direcion prueba',
+    //     birthday: new Date(),
+    //     contacts: ['Pedrito', 'Jorge'],
+    //     email: 'chinito@gmail.com',
+    //     lastname: 'Rodriguez',
+    //     name: 'Juan',
+    //     password: '123',
+    //     phonenumber: '5627572781',
+    //     role: 'Client',
+    // })
 
-    console.log(user)
+    // console.log(user)
 
-    //User.find({
-        //     name: 'Juan',
-        // })
+    // const users = await User.find({
+    //         name: 'Juan',
+    //     })
+
+    //     console.log(users)
     
     
-        // User.updateMany(
+        // const update = await User.updateMany(
         //     {
         //         name: 'Juan',
         //         lastname: 'Rodriguez',
@@ -40,7 +42,12 @@ mongoose.connect(process.env.DB_URI, async() => {
         //     }
         // )
     
-        // User.deleteOne({
-        //     name: 'Juan',
-        // })
+        //     console.log(update)
+
+
+        const deleteOne = await User.deleteOne({
+            name: 'Juan',
+        })
+
+        console.log(deleteOne)
 })
